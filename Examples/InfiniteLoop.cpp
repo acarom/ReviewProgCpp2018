10c7a7958641adedfa2f244ab2cda096f83b5765
Andrés Caro <36546774+acarom@users.noreply.github.com>
2022-12-05 23:34:57 +0100

    1st review

---
+++ Examples/InfiniteLoop.cpp
@@ -0,0 +1,9 @@
+// Infinite Loop || Bucle Infinito
+#include <iostream>
+using namespace std;
+int main(){
+  // for(int i=1; i>=1; i++){
+  //   cout<<"Value of variable i is: "<<i<<endl;
+   }
+   return 0;
+}
