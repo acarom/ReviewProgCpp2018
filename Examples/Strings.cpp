d20b0f4690f892df2adef33af8f68a2c7745c704
Andrés Caro <36546774+acarom@users.noreply.github.com>
2022-12-05 23:42:23 +0100

    1st review

---
+++ Examples/Strings.cpp
@@ -0,0 +1,17 @@
+// Strings - cadenas de informacion.
+#include <iostream>
+#include <string>
+
+int main ()
+
+{
+  std::string mystring;
+  
+  mystring = "lol";
+  std::cout << mystring << "\n";
+
+  mystring = "XD";
+  std::cout << mystring << "\n";
+
+  return 0;
+}
