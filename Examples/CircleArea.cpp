6da95330def1add06adf2cd52e22d45363203b63
Andrés Caro <36546774+acarom@users.noreply.github.com>
2022-12-05 23:47:59 +0100

    1st review

---
+++ Examples/CircleArea.cpp
@@ -0,0 +1,19 @@
+// Area of a circle and strings definitions || Area de un circulo con string y constantes (numeros y palabras). 
+#include <iostream>
+#include <string>
+
+const double pi = 3.14159;
+const std::string tab = "\t";
+const std::string nl= "\n";
+
+int main ()
+{
+  double r = 5.0;
+  double circle;
+  //area del circulo
+  circle = pi * r * r;
+    std::cout << circle << tab << "is the area of a circle" << nl;
+    std::cout << "of radius " << r << nl ;
+
+  return 0;
+}
